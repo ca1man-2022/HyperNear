0e3a402b44c540fe5b622291d5aba844c21db787
Tingyi <114342210+ca1man-2022@users.noreply.github.com>
2024-11-04 15:18:34 +0800

    Create FGA.sh

---
+++ FGA.sh
@@ -0,0 +1,19 @@
+python main_FGA.py --data coauthorship --dataset cora --model-name UniSAGE --gpu 1
+
+python main_FGA.py --data coauthorship --dataset dblp --model-name UniSAGE --gpu 1
+
+python main_FGA.py --data cocitation --dataset citeseer --model-name UniSAGE --gpu 1
+
+python main_FGA.py --data cocitation --dataset cora --model-name UniSAGE --gpu 1
+
+python main_FGA.py --data cocitation --dataset pubmed --model-name UniSAGE --gpu 1
+
+python main_FGA.py --data coauthorship --dataset cora --model-name UniGIN --gpu 1
+
+python main_FGA.py --data coauthorship --dataset dblp --model-name UniGIN --gpu 1
+
+python main_FGA.py --data cocitation --dataset citeseer --model-name UniGIN --gpu 1
+
+python main_FGA.py --data cocitation --dataset cora --model-name UniGIN --gpu 1
+
+python main_FGA.py --data cocitation --dataset pubmed --model-name UniGIN --gpu 1
